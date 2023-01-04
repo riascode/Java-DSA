d6db9b2e416fffe411761a6f21eb1767c28e3d77
riascode <86155955+riascode@users.noreply.github.com>
2023-01-05 00:18:25 +0530

    Create package_new.java

---
+++ package_new.java
@@ -0,0 +1,9 @@
+package  hello.java;
+
+public class main{
+  public static void main(String arg [])
+    System.out.print("This my learning journey");
+    System.out.println("This my learning journey \n this is a line break");
+  }
+}
+
