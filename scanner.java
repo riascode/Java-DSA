83089e8fbefd9ea2f90d0f4b8c35db7e0d4f7a72
riascode <86155955+riascode@users.noreply.github.com>
2023-01-05 00:36:29 +0530

    Create scanner.java

---
+++ scanner.java
@@ -0,0 +1,17 @@
+package hello.repository
+
+import java.util.*;
+
+public class Main{
+  public static void main(String[] args){
+    //input
+    Scanner sc = new Scanner(System.in);
+    String name = sc.next();
+    //sc.nextline()
+    //nextInt()
+    //nextFloat()
+    System.out.println(name);
+  }
+}
+
+    
