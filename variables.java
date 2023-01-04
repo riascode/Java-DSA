cfa7d353b2309a21931755a22d7bdc1a62e5513d
riascode <86155955+riascode@users.noreply.github.com>
2023-01-05 00:32:10 +0530

    Create variables.java (#2)

---
+++ variables.java
@@ -0,0 +1,16 @@
+package hello.repository;
+
+public class main {
+    public static void main(String[] args){
+      //variables
+      String name = "tony stark is still alive.";
+      int a = 10;
+      int age = 17
+      double price = 35.35
+      int b = 20;
+      
+      b=20;
+      name = "ironman"
+      
+    }
+}
